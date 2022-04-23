761a396f0ee368de74b41c521c037e7758096552
Émile <85419994+Emile-Massumov@users.noreply.github.com>
2022-04-22 21:07:42 -0500

    added data id for products

--- js/main.js
+++ js/main.js
@@ -2,9 +2,36 @@ let carts = document.querySelectorAll('.add-cart');
 
 for (let i=0; i < carts.length; i++) {
     carts[i].addEventListener ('click', () => {
-    cartNumbers();
+    cartNumbers(products[i]);
     })
 }
+
+let products = [
+    {
+        name: 'Versace Dylan Blue',
+        tag:'versacedylanblue',
+        price:'90',
+        inCart:0
+    },
+    {
+        name: 'ARMAF Le Club de Nuit',
+        tag:'armafleclubdenuit',
+        price:'54',
+        inCart:0
+    },
+    {
+        name: 'Armani Acqua Di Gio Profumo',
+        tag:'armaniacquadigioprofumo',
+        price:'110',
+        inCart:0
+    },
+    {
+        name: 'Guerlain LHomme Ideal EDP',
+        tag:'guerlainlhommeideal',
+        price:'110',
+        inCart:0
+    }
+];
 
 function onLoadCarts() {
     let productNumbers = localStorage.getItem('cartNumbers');
@@ -14,7 +41,8 @@ function onLoadCarts() {
 }
 }
 
-function cartNumbers() {
+function cartNumbers(product) { 
+    console.log('product here' , product);
     let productNumbers = localStorage.getItem('cartNumbers');
 
     productNumbers = parseInt(productNumbers);
@@ -26,5 +54,16 @@ if(productNumbers) {
     localStorage.setItem('cartNumbers',1);
     document.querySelector('.cart span').textContent = 1;
 }
+setItem(product);
+}
+
+function setItem(product) {
+   
+    product.inCart = 1;
+
+    cartItems = {
+        [product.tag]: product
+    }
+    localStorage.setItem("productInCart" , JSON.stringify(cartItems));
 }
 onLoadCarts();
