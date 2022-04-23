03211032cdf68521403a64f840f48cca7b90cd3b
Émile <85419994+Emile-Massumov@users.noreply.github.com>
2022-04-22 19:55:37 -0500

    added numbers to cart basket

--- js/main.js
+++ js/main.js
@@ -1,11 +1,30 @@
 let carts = document.querySelectorAll('.add-cart');
 
 for (let i=0; i < carts.length; i++) {
-    carts[0].addEventListener ('click', () => {
-        console.log("added carts");
+    carts[i].addEventListener ('click', () => {
+    cartNumbers();
     })
 }
 
-function cartNumbers() {
-    localStorage.setItem('cartNumbers', 1);
+function onLoadCarts() {
+    let productNumbers = localStorage.getItem('cartNumbers');
+    
+    if(productNumbers) { 
+    document.querySelector('.cart span').textContent = productNumbers;
 }
+}
+
+function cartNumbers() {
+    let productNumbers = localStorage.getItem('cartNumbers');
+
+    productNumbers = parseInt(productNumbers);
+if(productNumbers) {
+    localStorage.setItem('cartNumbers', productNumbers + 1);
+    document.querySelector('.cart span').textContent = productNumbers + 1;
+
+} else { 
+    localStorage.setItem('cartNumbers',1);
+    document.querySelector('.cart span').textContent = 1;
+}
+}
+onLoadCarts();
